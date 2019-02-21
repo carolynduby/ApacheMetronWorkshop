19a0c7634478a29035051cc8d680edeb1b5603b4
carolynduby <carolynduby@ieee.org>
2019-02-21 14:44:58 -0500

    add solr port to create collection
    Add solr port to create collection shell to differentiate between solr and solr infra.  Solr infra is the default.

--- build_single/typosquat/create_solr_collection.sh
+++ build_single/typosquat/create_solr_collection.sh
@@ -1,6 +1,5 @@
 export SOLR_HOME=/opt/lucidworks-hdpsearch/solr/
 export SOLR_USER=solr
 export METRON_HOME=/usr/hcp/current/metron
-export ZOOKEEPER=localhost:2181/solr
 cd $SOLR_HOME
-sudo -E su $SOLR_USER -c "bin/solr create_collection -c $1 -d $METRON_HOME/config/schema/$1/"
+sudo -E su $SOLR_USER -c "bin/solr create_collection -c $1 -d $METRON_HOME/config/schema/$1/ -p 8983"
