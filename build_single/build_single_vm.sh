f4cba5042605b1594fb6417a019b2aafa5e2e52e
root <root@localhost.localdomain>
2019-06-04 15:13:05 -0400

    add vm git setup

---
+++ build_single/build_single_vm.sh
@@ -0,0 +1,44 @@
+#install epel repo
+sudo yum -y install epel-release
+
+# install prereqs
+sudo yum install wget -y
+sudo yum install net-tools -y
+
+#install nodejs
+curl --silent --location https://rpm.nodesource.com/setup_8.x | sudo bash -
+yum install -y nodejs
+
+# correct the python-requests version so metron service status displays correct results
+sudo yum -y install python-pip
+sudo pip install requests==2.6.1
+sudo service ambari-agent restart
+
+## add autorestart command
+sudo cp start_service.sh /root
+sudo sh -c "echo 'bash /root/start_service.sh' >> /etc/rc.local"
+sudo chmod a+x /etc/rc.d/rc.local
+
+# set umask
+sudo umask 0022
+
+# configure the machine and install ambari
+export ambari_version="2.6.2.2"
+export install_ambari_server=true
+curl -sSL https://raw.githubusercontent.com/seanorama/ambari-bootstrap/master/ambari-bootstrap.sh | sudo -E sh
+ 
+wget -nv http://public-repo-1.hortonworks.com/HCP/centos7/1.x/updates/1.9.1.0/tars/metron/hcp-ambari-mpack-1.9.1.0-6.tar.gz 
+wget -nv http://public-repo-1.hortonworks.com/HCP/centos7/1.x/updates/1.9.1.0/tars/metron/elasticsearch_mpack-1.9.1.0-6.tar.gz 
+wget -nv http://public-repo-1.hortonworks.com/HDF/centos7/3.x/updates/3.1.2.0/tars/hdf_ambari_mp/hdf-ambari-mpack-3.1.2.0-7.tar.gz
+
+sudo ambari-server install-mpack --mpack=hcp-ambari-mpack-1.9.1.0-6.tar.gz --verbose
+sudo ambari-server install-mpack --mpack=elasticsearch_mpack-1.9.1.0-6.tar.gz --verbose
+sudo ambari-server install-mpack --mpack=hdf-ambari-mpack-3.1.2.0-7.tar.gz --verbose
+
+sudo ambari-server restart
+
+
+# install the demo content
+# don't run this yet, we need to setup metron with blueprint first
+# cd typosquat
+# ./install_squid_enrichments.sh
