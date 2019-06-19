73da56e0b6990e898b37f63b0c087f0f497cdc76
Cloud User <centos@ip-172-31-54-183.ec2.internal>
2019-06-19 01:06:32 +0000

    add squid elastic template

---
+++ build_single/post_install_setup.sh
@@ -0,0 +1,1 @@
+sudo -u hdfs /usr/hcp/current/metron/bin/maxmind_enrichment_load.sh -z localhost:2181
---
+++ build_single/typosquat/create_es_collection.sh
@@ -0,0 +1,1 @@
+curl -XPUT "http://localhost:9200/_template/mysquid" -H 'Content-Type: application/json' -d@$1
