a21c435dd1c172844eca5665612d68ae4f7ab3b3
root <root@ip-10-0-0-216.us-west-1.compute.internal>
2019-09-10 23:16:27 +0000

    make squid sensor initialization work on multiple node cluster

--- build_single/sensors/squid/post_ambari.sh
+++ build_single/sensors/squid/post_ambari.sh
@@ -6,6 +6,9 @@ popd () {
     command popd "$@" > /dev/null
 }
 
+source /etc/default/metron
+homedir=`getent passwd root | cut -d: -f6`
+
 # create the alexa 10 typosquat bloom filter
 ./create_typosquat_10k_filter.sh
 
@@ -13,7 +16,8 @@ popd () {
 sudo usermod -a -G squid nifi
 
 # install the domain whitelist
-cp ../../../04_TriagingSquid/scripts/* /home/centos
-pushd /home/centos
+cp ../../../04_TriagingSquid/scripts/* ${homedir} 
+pushd ${homedir} 
+sed "s/localhost:2181/${ZOOKEEPER}/" -i domain_whitelist_extractor_config.json
 ./import_domain_whitelist.sh
 popd
